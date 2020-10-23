fd2002237d0720a08ec4ef6adc5b48e9ff0c11f2
nigamalra <67359937+nigamalra@users.noreply.github.com>
2020-10-23 23:03:43 +0900

    마우스 키보드 - 수동적 처리

--- Resource.h
+++ Resource.h
@@ -1,17 +1,17 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// 다음에서 사용 FirstWindowsProgram.rc
+// 다음에서 사용 Passive_Input.rc
 
 #define IDS_APP_TITLE			103
 
 #define IDR_MAINFRAME			128
-#define IDD_FIRSTWINDOWSPROGRAM_DIALOG	102
+#define IDD_PASSIVEINPUT_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_FIRSTWINDOWSPROGRAM			107
+#define IDI_PASSIVEINPUT			107
 #define IDI_SMALL				108
-#define IDC_FIRSTWINDOWSPROGRAM			109
+#define IDC_PASSIVEINPUT			109
 #define IDC_MYICON				2
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
